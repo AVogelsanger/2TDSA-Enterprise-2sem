9b40a0630606164800260a18a33ea2f2dfd5558e
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-09 09:16:06 -0300

    Exerc 8

---
+++ Fiap08.Web.MVC/Fiap08.Web.MVC/Models/Patrocinador.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Fiap08.Web.MVC.Models
+{
+    public class Patrocinador
+    {
+        public int PatrocinadorId { get; set; }
+        public string nome { get; set; }
+        public IList<Projeto> Projetos { get; set; }
+    }
+}
